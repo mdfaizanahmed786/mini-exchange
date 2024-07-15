ad650b9e7e550a6e3c734a812389ffca940192cc
Faizan Ahmed <faizanzaid789@gmail.com>
2024-07-15 11:25:14 +0530

    modified middleware file

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,12 +1,29 @@
+import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+export async function middleware(request: NextRequest) {
+  const token=await getToken({req: request})
+  const url=request.nextUrl;
+ 
+  if(token 
+    &&
+    (url.pathname.startsWith("/api/auth/signin") || url.pathname.startsWith("/api/auth/signout"))
+
+  ){
+    return NextResponse.redirect(new URL('/', request.url))
+  
+  }
+  
+  if(!token && url.pathname.startsWith("/")){
+    return NextResponse.redirect(new URL('/api/auth/signin', request.url))
+  }
+
+  return NextResponse.next()
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/about/:path*',
+  matcher: ['/', '/api/:path*'],
 }
